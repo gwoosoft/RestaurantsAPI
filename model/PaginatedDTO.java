9fba3f4bff3d8511db57c6b622dd460fa117808a
Geonwoo Lee <71423299+gwoosoft@users.noreply.github.com>
2023-06-28 00:16:38 -0400

    Update PaginatedDTO.java

--- model/PaginatedDTO.java
+++ model/PaginatedDTO.java
@@ -5,16 +5,16 @@ import org.json.simple.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PaginatedDTO {
-    final Iterable<RestaurantDTO> items;
+public class PaginatedDTO<T> {
+    final Iterable<T> items;
     final String lastTokens;
 
-    public PaginatedDTO(Iterable<RestaurantDTO> items, String lastTokens) {
+    public PaginatedDTO(Iterable<T> items, String lastTokens) {
         this.items = items;
         this.lastTokens = lastTokens;
     }
 
-    public Iterable<RestaurantDTO> getItems() {
+    public Iterable<T> getItems() {
         return items;
     }
 
