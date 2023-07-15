6a749a7e37b7f2c4bdf38da599f0671a1f9ed04f
Geonwoo Lee <71423299+gwoosoft@users.noreply.github.com>
2023-07-15 13:52:25 -0400

    Create RestaurantAPIApplicationExceptionHandler.java

---
+++ main/java/advice/RestaurantAPIApplicationExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.gwsoft.restaurantAPI.advice;
+
+import com.gwsoft.restaurantAPI.error.CuisineNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class RestaurantAPIApplicationExceptionHandler {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(CuisineNotFoundException.class)
+    public Map<String, String> handleBusinessException(CuisineNotFoundException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public Map<String, String> handleBusinessException(RuntimeException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getMessage());
+        return errorMap;
+    }
+}
