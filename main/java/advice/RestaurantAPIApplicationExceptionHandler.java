39dad212f8c881557a9c80e97ff36f1ba549a054
Geonwoo Lee <71423299+gwoosoft@users.noreply.github.com>
2023-07-16 05:57:02 -0400

    Update RestaurantAPIApplicationExceptionHandler.java

--- main/java/advice/RestaurantAPIApplicationExceptionHandler.java
+++ main/java/advice/RestaurantAPIApplicationExceptionHandler.java
@@ -1,16 +1,50 @@
 package com.gwsoft.restaurantAPI.advice;
 
+import com.google.gson.stream.MalformedJsonException;
 import com.gwsoft.restaurantAPI.error.CuisineNotFoundException;
+import com.gwsoft.restaurantAPI.error.TokenMalformedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.UnsupportedEncodingException;
+import org.springframework.validation.BindException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class RestaurantAPIApplicationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public Map<String, String> handleInvalidArgument(BindException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public Map<String, String> handleInvalidArgument(NumberFormatException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getMessage());
+        return errorMap;
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(CuisineNotFoundException.class)
     public Map<String, String> handleBusinessException(CuisineNotFoundException ex) {
@@ -23,8 +57,32 @@ public class RestaurantAPIApplicationExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public Map<String, String> handleBusinessException(RuntimeException ex) {
         Map<String, String> errorMap = new HashMap<>();
-//        errorMap.put("errorMessage", ex.getMessage()); we should probably do not want to give actual backend error details to client
         errorMap.put("errorMessage", "it is unknown exception, please contact to server admin");
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UnsupportedEncodingException.class)
+    public Map<String, String> handleBusinessException(UnsupportedEncodingException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getLocalizedMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MalformedJsonException.class)
+    public Map<String, String> handleBusinessException(MalformedJsonException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getLocalizedMessage());
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(TokenMalformedException.class)
+    public Map<String, String> handleBusinessException(TokenMalformedException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", ex.getMessage());
+        errorMap.put("response", ex.getLocalizedMessage());
         return errorMap;
     }
 }
