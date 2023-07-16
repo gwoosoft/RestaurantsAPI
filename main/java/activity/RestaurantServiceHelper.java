5df7b110a49322df9998d085e87f6484b22b4e20
Geonwoo Lee <71423299+gwoosoft@users.noreply.github.com>
2023-07-16 01:50:48 -0400

    Update RestaurantServiceHelper.java

--- main/java/activity/RestaurantServiceHelper.java
+++ main/java/activity/RestaurantServiceHelper.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.QueryResultPage;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.MalformedJsonException;
 import com.gwsoft.restaurantAPI.model.CuisineDTO;
 import com.gwsoft.restaurantAPI.model.RestaurantDTO;
 import com.gwsoft.restaurantAPI.model.RestaurantEntity;
@@ -35,17 +36,30 @@ public class RestaurantServiceHelper {
         return cuisineDtoList;
     }
 
-    public Map<String, AttributeValue> getStartToken(String lastEvaluatedKey){
-        String decode64Value=null;
+    public Map<String, AttributeValue> getStartToken(String lastEvaluatedKey) throws MalformedJsonException {
+        String decode64Value = null;
 
         if(lastEvaluatedKey!=null){
             decode64Value= new String(Base64.decodeBase64(lastEvaluatedKey), Charset.forName("UTF-8"));
         }
-        Map<String, AttributeValue> startToken = gsonHelper.fromJson(decode64Value, new TypeToken<HashMap<String, AttributeValue>>(){}.getType());
-        return startToken;
+
+        try{
+            return gsonHelper.fromJson(decode64Value, new TypeToken<HashMap<String, AttributeValue>>(){}.getType());
+        }catch (Exception e){
+            if(e instanceof MalformedJsonException){
+                throw new MalformedJsonException("Error during the decoding lastEvaluated Key, please check if it is correct form");
+            }else{
+                e.printStackTrace();
+                throw new RuntimeException("Unknown error incurred during the encoding process");
+            }
+        }
     }
 
     public String getEncodedLastEvaluatedKey(Map<String, AttributeValue> lastEvaluatedKey) throws UnsupportedEncodingException {
-        return Base64.encodeBase64String(gsonHelper.toJson(lastEvaluatedKey).getBytes("UTF-8"));
+        try{
+            return Base64.encodeBase64String(gsonHelper.toJson(lastEvaluatedKey).getBytes("UTF-8"));
+        }catch (UnsupportedEncodingException e){
+            throw new UnsupportedEncodingException("You inserted wrong lastEvaluatedKey in unsupported encoding format, please check again");
+        }
     }
 }
