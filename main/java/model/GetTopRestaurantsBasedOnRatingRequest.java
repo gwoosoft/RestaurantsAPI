8e0b3dbbc5d9f2d8d394a2b9739003f1a05283cb
Geonwoo Lee <71423299+gwoosoft@users.noreply.github.com>
2023-07-17 00:40:42 -0400

    Update GetTopRestaurantsBasedOnRatingRequest.java

--- main/java/model/GetTopRestaurantsBasedOnRatingRequest.java
+++ main/java/model/GetTopRestaurantsBasedOnRatingRequest.java
@@ -6,19 +6,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
 public class GetTopRestaurantsBasedOnRatingRequest extends BaseRequest {
-    @Pattern(regexp = "^\\d+(\\.\\d+)?$", message = "Please only insert digits")
-    @NotNull(message = "rating should not be null")
-    @Min(0)
-    @Max(5)
-    private String rating;
+    @NotNull(message = "the value cannot be empty")
+    @DecimalMin("0")
+    @DecimalMax("5")
+    private Double  rating;
 }
